1cf1a9123a035b68f42418f5a6f5e79d6be45c6a
efrain <efragal.20@gmail.com>
2024-10-07 18:58:36 -0500

    create board of the game

---
+++ board.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+class Board():
+    def __init__(self):
+        super().__init__()
+        self.screen = Screen()
+        self.drawer = Turtle()
+        self.screen_size = {"x": 800, "y": 600}
+        self.line_lenght = 10
+        self.line_width = 10
+
+    def draw_field_pong(self):
+        self.screen.setup(width=self.screen_size["x"], height = self.screen_size["y"],startx=400, starty=200)
+        self.screen.bgcolor("black")
+        self.drawer.up()
+        self.drawer.color("white")
+        self.drawer.setpos(0, self.screen_size["y"]/2)
+        self.drawer.seth(270)
+        self.drawer.width = self.line_width
+        for segment in range(0,100):
+            self.drawer.down()
+            self.drawer.forward(self.line_lenght)
+            self.drawer.up()
+            self.drawer.forward(self.line_lenght)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from board import Board
+
+pong_game_board = Board()
+pong_game_board.draw_field_pong()
+
+pong_game_board.screen.exitonclick()
