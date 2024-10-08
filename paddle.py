e55c4f3236fdef07683d506ba36ed7c99fe533df
efrain <efragal.20@gmail.com>
2024-10-07 19:04:11 -0500

    create paddle class

---
+++ paddle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class Paddle():
+    def __init__(self):
+        self.size = 4
+    
+    def create_paddle(self, side):
+        if side.lower() == "left":
+            
+        elif side == "right":
+
+        else
+            print(f"Wrong side: {side}")
